b31031325c8a271a93bb741e242f5df5158c9c6c
BrealX <lyoxxxa1985@gmail.com>
2018-01-20 10:55:37 +0200

    alpha

---
+++ alpha/orders/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from .models import *
+
+
+class ProductInOrderInline(admin.TabularInline):
+    model = ProductInOrder
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Order
+
+    list_display = [field.name for field in Order._meta.fields]
+    list_filter = ['name', 'id']
+    search_fields = ['name', 'id']
+    inlines = [ProductInOrderInline]
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Status
+
+    list_display = [field.name for field in Status._meta.fields]
+
+
+admin.site.register(Status, StatusAdmin)
+
+
+class ProductInOrderAdmin(admin.ModelAdmin):
+    class Meta:
+        model = ProductInOrder
+
+    list_display = [field.name for field in ProductInOrder._meta.fields]
+
+
+admin.site.register(ProductInOrder, ProductInOrderAdmin)
---
+++ alpha/products/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import *
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Product
+
+    list_display = [field.name for field in Product._meta.fields]
+    list_filter = ['name', 'id']
+    search_fields = ['name', 'id']
+    inlines = [ProductImageInline]
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductInOrderAdmin(admin.ModelAdmin):
+    class Meta:
+        model = ProductImage
+
+    list_display = [field.name for field in ProductImage._meta.fields]
+
+
+admin.site.register(ProductImage, ProductInOrderAdmin)
