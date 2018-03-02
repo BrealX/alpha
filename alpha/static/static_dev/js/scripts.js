5c8aa2f1b399cfb7744c3ac96502c738a5d44ff5
BrealX <lyoxxxa1985@gmail.com>
2018-03-03 01:46:23 +0200

    landing page creating, added javascript on scroll

--- alpha/static/static_dev/js/scripts.js
+++ alpha/static/static_dev/js/scripts.js
@@ -205,6 +205,31 @@ $(document).ready(function() {
 
     calculatingTotalBasketAmount();
 
+    /*// Helper function for add element box list in WOW
+    WOW.prototype.addBox = function(element) {
+        this.boxes.push(element);
+    };*/
+
+    window.addEventListener('scroll', function(e) {
+        if( $(window).scrollTop() <= 50) {
+            $('.wow').removeClass('animated');
+            $('.wow').removeAttr('style');
+            new WOW().init();
+        }
+    });
+
+    /*// WOW Effects Initializer
+    new WOW().init();*/
+
+    /*// Attach scrollSpy to .wow elements for detect view exit events,
+    // then reset elements and add again for animation
+    $('.wow').on('scrollSpy:exit', function() {
+        $(this).css({
+            'visibility': 'hidden',
+            'animation-name': 'none'
+        }).removeClass('animated');
+    wow.addBox(this);
+    }).scrollSpy();*/
 
     // Home Page Slider Initializer
     $('.owl-carousel').owlCarousel({
