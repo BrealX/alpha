6fb126294d446c524d543736250444d693f61957
BrealX <lyoxxxa1985@gmail.com>
2018-04-11 11:26:16 +0300

    context processor + truncatechars

--- alpha/landing/context_processors.py
+++ alpha/landing/context_processors.py
@@ -5,9 +5,10 @@ def navigation_bar(request):
     nav_products_microphones = Product.objects.filter(is_active=True, category__id=1)
     nav_products_players = Product.objects.filter(is_active=True, category__id=2)
     nav_categories = ProductCategory.objects.filter(is_active=True)
-    products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
-    products_images_microphones = products_images.filter(product__category__id=1)
-    products_images_players = products_images.filter(product__category__id=2)
+    all_products = Product.objects.filter(is_active=True)
+    new_arrivals = all_products.order_by('-created')
+    for product in new_arrivals:
+        pass
     user = request.user
     if user.is_authenticated:
         if user.first_name:
